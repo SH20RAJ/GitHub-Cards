3feeedde5ae3ff6171ba6f40ff04c17e792ae52a
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-02-02 11:50:44 +0000

    Animations + Buttons Added

--- githubcards.js
+++ githubcards.js
@@ -4,7 +4,7 @@ if(getparam('user')){
 }
 
 function embed(){
-  
+  window.prompt('Copy the iframe code.','<iframe src="https://github-cards.sh20raj.repl.co/embed.html?user='+ document.getElementById('user').value+'" height="1000px" width="100%"></iframe>');
 }
 
 var url = 'https://api.github.com/users/' + user + '/repos';
@@ -16,7 +16,7 @@ fetch(url)
 function showrepos(data) {
   for (let i = 0; i < data.length; i++) {
     field.insertAdjacentHTML('beforeend', `
-   <div class="github-card">
+   <div class="github-card bounce-bottom">
    <a href="https://github.com/${data[i].full_name}" target="_blank">
    <img src="https://gh-card.dev/repos/${data[i].full_name}.svg"/></a>
    </div>
